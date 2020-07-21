ca9e37b3ed802ee443dbfc89cf3a78e285ffb7f8
LegendaryZotar <62851085+LegendaryZotar@users.noreply.github.com>
2020-07-21 23:13:04 +0300

    Initial commit

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,32 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public CinemachineFreeLook CFreeLock;
+    public CinemachineBrain CBrain;
+
+    public float XSens;
+    public float YSens;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        //Toggles cursor lock using the ESC key.
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+        //Multiplying by 300f for the "steps" to be the same as YSens
+        CFreeLock.m_XAxis.m_MaxSpeed = XSens * 300f;
+        CFreeLock.m_YAxis.m_MaxSpeed = YSens;
+        CBrain.enabled = Cursor.lockState == CursorLockMode.Locked;
+    }
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float WalkSpeed = 6f;
+    public float RunSpeed = 10f;
+    float speed;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+            speed = RunSpeed;
+        else
+            speed = WalkSpeed;
+
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f && Cursor.lockState == CursorLockMode.Locked)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
